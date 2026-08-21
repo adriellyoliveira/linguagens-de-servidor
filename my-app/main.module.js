f6ed422f9fe18d7a168ad537292f90b5bc7df765
Adrielly Araujo <adriellyaraujo826@gmail.com>
2026-08-21 08:40:29 -0300

    Cria: arquivo main.module.js

---
+++ my-app/main.module.js
@@ -0,0 +1,24 @@
+import chalk from "chalk"
+import { log } from "console"
+import read from "readline-sync"
+
+//nome, sobrenome, idade, altura, peso e bairro
+// let nome = read.question("Digite seu nome: ")
+//let sobrenome = read.question("Digite seu sobrenome: ")
+//let idade = read.question("Digite sua idade: ")
+//let altura = read.question("Digite sua altura: ")
+//let peso = read.question("Digite seu peso: ")
+//let bairro = read.question("Digite seu bairro: ")
+
+//console.log(chalk.bgBlack.white(`Olá, ${nome} ${sobrenome}! Você tem ${idade} anos, sua altura é ${altura}m, seu peso é ${peso}kg e mora em ${bairro}. Prazer em conhecer!`));
+
+// criar um arquivo areaRetangulo.js, solicitar o valor da altura e largura e entregar para ele a area e perimetro
+
+soma = 2 + 2
+sub = 2 - 2
+div = 2 / 2
+mult = 2 * 2
+pot = 2 ** 2
+
+areaR = 100 * 20
+periR = (100 * 2) + (20 * 2)
